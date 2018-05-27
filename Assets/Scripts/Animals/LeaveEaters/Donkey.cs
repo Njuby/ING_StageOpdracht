be7a727df932495f09436a00054be7310433a6e5
David_Elout <dedavid.elout@gmail.com>
2018-05-27 21:29:19 +0200

    Added donkey

---
+++ Assets/Scripts/Animals/LeaveEaters/Donkey.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Donkey : Animal
+{
+    [SerializeField]
+    private GameObject Balloon;
+    [SerializeField]
+    private Text text;
+    // public string name;
+
+
+    public override void SayHello()
+    {
+        Balloon.SetActive(true);
+        text.text = "donkey donkey";
+    }
+
+    public override void EatLeaves()
+    {
+        Balloon.SetActive(true);
+        text.text = "nom nom nom ";
+    }
+}
