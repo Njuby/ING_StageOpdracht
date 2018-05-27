dbab4744dd21bccd31b422250f2f89a60a727b2b
David_Elout <dedavid.elout@gmail.com>
2018-05-27 23:31:43 +0200

    Added elephant

---
+++ Assets/Scripts/Animals/LeaveEaters/Elephant.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Elephant : Animal {
+
+    [SerializeField]
+    private GameObject Balloon;
+    [SerializeField]
+    private Text text;
+    // public string name;
+
+
+    public override void SayHello()
+    {
+        Balloon.SetActive(true);
+        text.text = "TUUT TUUT";
+    }
+
+    public override void EatLeaves()
+    {
+        Balloon.SetActive(true);
+        text.text = "SGHRUNCH SGHRUNCH";
+    }
+}
